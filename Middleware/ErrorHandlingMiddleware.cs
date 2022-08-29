9de3bf528d7cf72e41898aa4fe25b20f67c527ba
Vladyslav Zaiets <rutova2@gmail.com>
2022-08-29 21:08:44 +0300

    refactor(backgroundworkers): reshuffle ServiceStatusUpdateWorker a bit

---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,77 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Net;
+using System.Text.Json;
+using SystemdServiceMonitor.Responses;
+
+namespace SystemdServiceMonitor.Middleware;
+
+/// <summary>
+/// Global error handling middleware that catches unhandled exceptions and returns consistent error responses.
+/// Prevents internal server details from leaking to clients while maintaining detailed logging.
+/// </summary>
+public class ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await context.Next();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception in request pipeline. Path: {Path}", context.Request.Path);
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        var response = new ApiResponse<object>
+        {
+            Success = false,
+            TraceId = context.TraceIdentifier,
+            Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds()
+        };
+
+        // Determine status code and message based on exception type
+        (int statusCode, string message) = exception switch
+        {
+            ArgumentNullException => (StatusCodes.Status400BadRequest, "Required parameter is missing"),
+            ArgumentException => (StatusCodes.Status400BadRequest, "Invalid argument provided"),
+            UnauthorizedAccessException => (StatusCodes.Status403Forbidden, "Access denied"),
+            OperationCanceledException => (StatusCodes.Status408RequestTimeout, "Request was cancelled"),
+            TimeoutException => (StatusCodes.Status408RequestTimeout, "Request timeout"),
+            InvalidOperationException => (StatusCodes.Status400BadRequest, "Invalid operation"),
+            _ => (StatusCodes.Status500InternalServerError, "An internal error occurred")
+        };
+
+        context.Response.StatusCode = statusCode;
+        response.Message = message;
+
+        // Only include error details in development
+        if (context.RequestServices.GetRequiredService<IHostEnvironment>().IsDevelopment())
+        {
+            response.ErrorDetails = exception.ToString();
+        }
+
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        return context.Response.WriteAsJsonAsync(response, options);
+    }
+}
+
+/// <summary>
+/// Extension method to register the error handling middleware in the request pipeline.
+/// </summary>
+public static class ErrorHandlingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ErrorHandlingMiddleware>();
+    }
+}
