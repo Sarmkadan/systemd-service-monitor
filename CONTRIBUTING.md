06fb322650e10006faac88b86b01bd26ff91d975
Vladyslav Zaiets <rutova2@gmail.com>
2022-10-14 11:30:37 +0300

    cleanup in IServiceDependencyGraphService.

---
+++ CONTRIBUTING.md
@@ -0,0 +1,19 @@
+# Contributing to systemd-service-monitor
+
+Thank you for your interest in contributing!
+
+## How to Contribute
+
+1. **Fork the repository** on GitHub.
+2. **Create a branch** for your feature or fix.
+3. **Write tests** for any changes you make.
+4. **Ensure the build passes** by running `dotnet build` and `dotnet test`.
+5. **Submit a Pull Request** to the `main` branch.
+
+## Standards
+
+- Follow the existing coding style (see `.editorconfig`).
+- Ensure all public APIs have XML documentation.
+- Maintain consistency with the existing project structure.
+
+By contributing, you agree to license your contributions under the MIT License.
--- Services/IServiceLogService.cs
+++ Services/IServiceLogService.cs
@@ -12,57 +12,98 @@ public interface IServiceLogService
     /// <summary>
     /// Retrieves recent logs for a specific service.
     /// </summary>
+    /// <param name="unitName">The name of the systemd unit.</param>
+    /// <param name="limit">The maximum number of logs to retrieve.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A collection of service logs.</returns>
     Task<IEnumerable<ServiceLog>> GetServiceLogsAsync(string unitName, int limit = 100, CancellationToken ct = default);
 
     /// <summary>
     /// Retrieves logs within a specific time range.
     /// </summary>
+    /// <param name="unitName">The name of the systemd unit.</param>
+    /// <param name="from">The start of the time range.</param>
+    /// <param name="to">The end of the time range.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A collection of service logs within the specified time range.</returns>
     Task<IEnumerable<ServiceLog>> GetLogsInTimeRangeAsync(string unitName, DateTime from, DateTime to, CancellationToken ct = default);
 
     /// <summary>
     /// Retrieves logs by severity level.
     /// </summary>
+    /// <param name="unitName">The name of the systemd unit.</param>
+    /// <param name="level">The syslog severity level to filter by.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A collection of service logs matching the severity level.</returns>
     Task<IEnumerable<ServiceLog>> GetLogsByLevelAsync(string unitName, SyslogLevel level, CancellationToken ct = default);
 
     /// <summary>
     /// Retrieves the most recent logs across all services.
     /// </summary>
+    /// <param name="limit">The maximum number of logs to retrieve.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A collection of recent service logs.</returns>
     Task<IEnumerable<ServiceLog>> GetRecentLogsAsync(int limit = 100, CancellationToken ct = default);
 
     /// <summary>
     /// Retrieves logs matching a search term.
     /// </summary>
+    /// <param name="searchTerm">The search term to match against logs.</param>
+    /// <param name="limit">The maximum number of logs to retrieve.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A collection of service logs matching the search term.</returns>
     Task<IEnumerable<ServiceLog>> SearchLogsAsync(string searchTerm, int limit = 100, CancellationToken ct = default);
 
     /// <summary>
     /// Fetches latest logs directly from systemd journald.
     /// </summary>
+    /// <param name="unitName">The name of the systemd unit.</param>
+    /// <param name="count">The maximum number of logs to retrieve.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A collection of latest logs fetched from the systemd journal.</returns>
     Task<IEnumerable<ServiceLog>> FetchLatestFromJournalAsync(string unitName, int count = 50, CancellationToken ct = default);
 
     /// <summary>
     /// Fetches latest logs from systemd journald filtered by minimum syslog priority.
     /// Only entries at <paramref name="minimumPriority"/> or higher severity are returned.
     /// </summary>
+    /// <param name="unitName">The name of the systemd unit.</param>
+    /// <param name="minimumPriority">The minimum syslog priority to include.</param>
+    /// <param name="count">The maximum number of logs to retrieve.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>A collection of logs matching the priority criteria.</returns>
     Task<IEnumerable<ServiceLog>> FetchFromJournalByPriorityAsync(string unitName, SyslogLevel minimumPriority, int count = 50, CancellationToken ct = default);
 
     /// <summary>
     /// Stores a log entry in the repository.
     /// </summary>
+    /// <param name="log">The log entry to store.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>The stored service log.</returns>
     Task<ServiceLog> StoreLogAsync(ServiceLog log, CancellationToken ct = default);
 
     /// <summary>
     /// Stores multiple log entries in batch.
     /// </summary>
+    /// <param name="logs">The collection of log entries to store.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>The number of logs stored.</returns>
     Task<int> StoreLogsAsync(IEnumerable<ServiceLog> logs, CancellationToken ct = default);
 
     /// <summary>
     /// Clears logs older than specified days.
     /// </summary>
+    /// <param name="retentionDays">The number of days to retain logs.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>The number of logs cleared.</returns>
     Task<int> ClearOldLogsAsync(int retentionDays, CancellationToken ct = default);
 
     /// <summary>
     /// Gets log statistics for a service.
     /// </summary>
+    /// <param name="unitName">The name of the systemd unit.</param>
+    /// <param name="ct">The cancellation token.</param>
+    /// <returns>The log statistics for the service.</returns>
     Task<LogStatistics> GetLogStatisticsAsync(string unitName, CancellationToken ct = default);
 }
 
