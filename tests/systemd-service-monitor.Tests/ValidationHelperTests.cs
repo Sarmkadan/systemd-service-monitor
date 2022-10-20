4ce6c610c9bf474404e317647518d9925c74166a
Vladyslav Zaiets <rutova2@gmail.com>
2022-10-20 15:37:16 +0300

    guard clauses in ValidationHelper

--- tests/systemd-service-monitor.Tests/ValidationHelperTests.cs
+++ tests/systemd-service-monitor.Tests/ValidationHelperTests.cs
@@ -4,10 +4,15 @@ using FluentAssertions;
 using SystemdServiceMonitor.Utilities;
 using Xunit;
 
-namespace SystemdServiceMonitor.Tests;
-
+/// <summary>
+/// Tests for the ValidationHelper class.
+/// </summary>
 public class ValidationHelperTests
 {
+    /// <summary>
+    /// Tests that the ValidateServiceName method returns a valid result for valid service names.
+    /// </summary>
+    /// <param name="name">The service name to validate.</param>
     [Theory]
     [InlineData("nginx.service")]
     [InlineData("my-daemon.service")]
@@ -21,6 +26,10 @@ public class ValidationHelperTests
         result.IsValid.Should().BeTrue();
     }
 
+    /// <summary>
+    /// Tests that the ValidateServiceName method returns an invalid result with a message for invalid service names.
+    /// </summary>
+    /// <param name="name">The service name to validate.</param>
     [Theory]
     [InlineData("my service")]
     [InlineData("bad$name")]
@@ -36,6 +45,10 @@ public class ValidationHelperTests
         result.ErrorMessage.Should().NotBeNullOrEmpty();
     }
 
+    /// <summary>
+    /// Tests that the ValidatePort method returns an invalid result for out-of-bounds ports.
+    /// </summary>
+    /// <param name="port">The port to validate.</param>
     [Theory]
     [InlineData(0)]
     [InlineData(65536)]
@@ -50,6 +63,10 @@ public class ValidationHelperTests
         result.ErrorMessage.Should().Contain("65535");
     }
 
+    /// <summary>
+    /// Tests that the ValidatePort method returns a valid result for valid ports.
+    /// </summary>
+    /// <param name="port">The port to validate.</param>
     [Theory]
     [InlineData(1)]
     [InlineData(80)]
@@ -63,7 +80,9 @@ public class ValidationHelperTests
         result.IsValid.Should().BeTrue();
     }
 
-    [Fact]
+    /// <summary>
+    /// Tests that the ValidateTimeRange method returns an invalid result when the end date exceeds the start date by more than one year.
+    /// </summary>
     public void ValidateTimeRange_ExceedsOneYear_ReturnsInvalid()
     {
         // Arrange
@@ -78,7 +97,9 @@ public class ValidationHelperTests
         result.ErrorMessage.Should().Contain("365");
     }
 
-    [Fact]
+    /// <summary>
+    /// Tests that the ValidateTimeRange method returns an invalid result when the start date is after the end date.
+    /// </summary>
     public void ValidateTimeRange_StartAfterEnd_ReturnsInvalid()
     {
         // Arrange
@@ -92,7 +113,9 @@ public class ValidationHelperTests
         result.IsValid.Should().BeFalse();
     }
 
-    [Fact]
+    /// <summary>
+    /// Tests that the ValidateUrl method returns an invalid result for URLs with the FTP scheme.
+    /// </summary>
     public void ValidateUrl_FtpScheme_ReturnsInvalid()
     {
         // Act
@@ -103,6 +126,12 @@ public class ValidationHelperTests
         result.ErrorMessage.Should().Contain("HTTP");
     }
 
+    /// <summary>
+    /// Tests that the SanitizeInput method truncates input strings that exceed the maximum length.
+    /// </summary>
+    /// <param name="input">The input string to sanitize.</param>
+    /// <param name="maxLength">The maximum length of the sanitized string.</param>
+    /// <returns>The sanitized string.</returns>
     [Fact]
     public void SanitizeInput_StringExceedingMaxLength_TruncatesToLimit()
     {
