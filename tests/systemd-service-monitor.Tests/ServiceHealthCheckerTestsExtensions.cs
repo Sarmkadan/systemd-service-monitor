551c651dad091b081b51eca9b21a17116af5504e
Vladyslav Zaiets <rutova2@gmail.com>
2022-12-08 08:27:08 +0200

    Add extension helpers for ServiceHealthChecker

--- tests/systemd-service-monitor.Tests/ServiceHealthCheckerTestsExtensions.cs
+++ tests/systemd-service-monitor.Tests/ServiceHealthCheckerTestsExtensions.cs
@@ -15,6 +15,9 @@ namespace SystemdServiceMonitor.Tests
         /// Executes the <c>GetHealthStatus_NullService_ReturnsUnknown</c> test and returns <c>true</c>
         /// if it completes without throwing an exception, otherwise <c>false</c>.
         /// </summary>
+        /// <param name="tests">The test instance to execute the method on.</param>
+        /// <returns><c>true</c> if the test completes without throwing an exception; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <c>null</c>.</exception>
         public static bool VerifyNullService(this ServiceHealthCheckerTests tests) =>
             RunTest(() => tests.GetHealthStatus_NullService_ReturnsUnknown());
 
@@ -22,6 +25,9 @@ namespace SystemdServiceMonitor.Tests
         /// Executes the <c>GetHealthStatus_ActiveAndStableService_ReturnsHealthy</c> test and returns
         /// <c>true</c> if it succeeds, otherwise <c>false</c>.
         /// </summary>
+        /// <param name="tests">The test instance to execute the method on.</param>
+        /// <returns><c>true</c> if the test completes without throwing an exception; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <c>null</c>.</exception>
         public static bool VerifyActiveAndStableService(this ServiceHealthCheckerTests tests) =>
             RunTest(() => tests.GetHealthStatus_ActiveAndStableService_ReturnsHealthy());
 
@@ -30,8 +36,13 @@ namespace SystemdServiceMonitor.Tests
         /// <c>GetHealthStatus_</c> and returns a dictionary mapping the method name to a boolean that
         /// indicates whether the method completed without throwing.
         /// </summary>
+        /// <param name="tests">The test instance to execute the methods on.</param>
+        /// <returns>A dictionary mapping method names to boolean results indicating test success.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="tests"/> is <c>null</c>.</exception>
         public static IDictionary<string, bool> RunAllHealthStatusTests(this ServiceHealthCheckerTests tests)
         {
+            ArgumentNullException.ThrowIfNull(tests);
+
             var result = new Dictionary<string, bool>(StringComparer.Ordinal);
             var methods = typeof(ServiceHealthCheckerTests)
                 .GetMethods(BindingFlags.Instance | BindingFlags.Public)
@@ -59,8 +70,13 @@ namespace SystemdServiceMonitor.Tests
         /// <summary>
         /// Helper that executes an <see cref="Action"/> and returns <c>true</c> if no exception is thrown.
         /// </summary>
+        /// <param name="test">The test action to execute.</param>
+        /// <returns><c>true</c> if the action completes without throwing; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="test"/> is <c>null</c>.</exception>
         private static bool RunTest(Action test)
         {
+            ArgumentNullException.ThrowIfNull(test);
+
             try
             {
                 test();
