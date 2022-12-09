c0f2381eae138e48ae7d912ed8b31fea71a1d1fd
Vladyslav Zaiets <rutova2@gmail.com>
2022-12-09 18:33:17 +0200

    .

--- Extensions/DateTimeExtensions.cs
+++ Extensions/DateTimeExtensions.cs
@@ -12,21 +12,28 @@ public static class DateTimeExtensions
     /// Calculates the time elapsed since the given datetime.
     /// Returns a human-readable string like "2 hours ago" or "3 days ago".
     /// </summary>
+    /// <param name="dateTime">The reference DateTime to calculate relative time from.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="dateTime"/> is in the future.</exception>
     public static string ToRelativeTime(this DateTime dateTime)
     {
+        if (dateTime > DateTime.UtcNow)
+        {
+            throw new ArgumentOutOfRangeException(nameof(dateTime), "DateTime cannot be in the future.");
+        }
+
         var timeSpan = DateTime.UtcNow - dateTime;
 
         return timeSpan.TotalSeconds < 60
-            ? $"{(int)timeSpan.TotalSeconds} seconds ago"
+            ? $"{(int)timeSpan.TotalSeconds} second{(timeSpan.TotalSeconds == 1 ? "" : "s")} ago"
             : timeSpan.TotalMinutes < 60
-            ? $"{(int)timeSpan.TotalMinutes} minutes ago"
-            : timeSpan.TotalHours < 24
-            ? $"{(int)timeSpan.TotalHours} hours ago"
-            : timeSpan.TotalDays < 30
-            ? $"{(int)timeSpan.TotalDays} days ago"
-            : timeSpan.TotalDays < 365
-            ? $"{(int)(timeSpan.TotalDays / 30)} months ago"
-            : $"{(int)(timeSpan.TotalDays / 365)} years ago";
+                ? $"{(int)timeSpan.TotalMinutes} minute{(timeSpan.TotalMinutes == 1 ? "" : "s")} ago"
+                : timeSpan.TotalHours < 24
+                    ? $"{(int)timeSpan.TotalHours} hour{(timeSpan.TotalHours == 1 ? "" : "s")} ago"
+                    : timeSpan.TotalDays < 30
+                        ? $"{(int)timeSpan.TotalDays} day{(timeSpan.TotalDays == 1 ? "" : "s")} ago"
+                        : timeSpan.TotalDays < 365
+                            ? $"{(int)(timeSpan.TotalDays / 30)} month{(timeSpan.TotalDays / 30 == 1 ? "" : "s")} ago"
+                            : $"{(int)(timeSpan.TotalDays / 365)} year{(timeSpan.TotalDays / 365 == 1 ? "" : "s")} ago";
     }
 
     /// <summary>
@@ -72,8 +79,17 @@ public static class DateTimeExtensions
     /// <summary>
     /// Returns true if the given datetime is within the specified time range.
     /// </summary>
+    /// <param name="dateTime">The DateTime to check.</param>
+    /// <param name="startDate">The start of the time range (inclusive).</param>
+    /// <param name="endDate">The end of the time range (inclusive).</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="startDate"/> is after <paramref name="endDate"/>.</exception>
     public static bool IsWithinRange(this DateTime dateTime, DateTime startDate, DateTime endDate)
     {
+        if (startDate > endDate)
+        {
+            throw new ArgumentOutOfRangeException(nameof(startDate), "Start date cannot be after end date.");
+        }
+
         return dateTime >= startDate && dateTime <= endDate;
     }
 
@@ -81,8 +97,16 @@ public static class DateTimeExtensions
     /// Rounds DateTime to the nearest specified interval.
     /// Useful for grouping log entries or metrics by time buckets.
     /// </summary>
+    /// <param name="dateTime">The DateTime to round.</param>
+    /// <param name="interval">The time interval to round to.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="interval"/> is zero or negative.</exception>
     public static DateTime RoundToNearest(this DateTime dateTime, TimeSpan interval)
     {
+        if (interval <= TimeSpan.Zero)
+        {
+            throw new ArgumentOutOfRangeException(nameof(interval), "Interval must be positive.");
+        }
+
         var totalTicks = dateTime.Ticks / (double)interval.Ticks;
         var roundedTicks = Math.Round(totalTicks) * interval.Ticks;
         return new DateTime((long)roundedTicks);
@@ -123,14 +147,15 @@ public static class DateTimeExtensions
     /// <summary>
     /// Formats a timespan as a human-readable string.
     /// </summary>
+    /// <param name="timeSpan">The TimeSpan to format.</param>
     public static string ToHumanReadableString(this TimeSpan timeSpan)
     {
-        if (timeSpan.TotalSeconds < 60)
-            return $"{(int)timeSpan.TotalSeconds}s";
-        if (timeSpan.TotalMinutes < 60)
-            return $"{(int)timeSpan.TotalMinutes}m";
-        if (timeSpan.TotalHours < 24)
-            return $"{(int)timeSpan.TotalHours}h";
-        return $"{(int)timeSpan.TotalDays}d";
+        return timeSpan.TotalSeconds < 60
+            ? $"{(int)timeSpan.TotalSeconds}s"
+            : timeSpan.TotalMinutes < 60
+                ? $"{(int)timeSpan.TotalMinutes}m"
+                : timeSpan.TotalHours < 24
+                    ? $"{(int)timeSpan.TotalHours}h"
+                    : $"{(int)timeSpan.TotalDays}d";
     }
 }
