2658b267e128ea448a48da24d8e7118034ec9e9d
Vladyslav Zaiets <rutova2@gmail.com>
2022-12-16 16:19:49 +0200

    feat: extension methods on ServiceLog

--- Models/ServiceLogExtensions.cs
+++ Models/ServiceLogExtensions.cs
@@ -9,12 +9,16 @@ namespace SystemdServiceMonitor.Models;
 /// Extension methods for <see cref="ServiceLog"/> providing utility functionality
 /// for filtering, formatting, and analyzing service log entries.
 /// </summary>
+/// <remarks>
+/// All methods validate input parameters and throw appropriate exceptions for null or invalid values.
+/// </remarks>
 public static class ServiceLogExtensions
 {
     /// <summary>
     /// Determines whether this log entry represents an error or higher severity level.
     /// </summary>
     /// <param name="log">The service log entry to check.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="log"/> is <see langword="null"/>.</exception>
     /// <returns>True if the log level is Error, Critical, Alert, or Emergency; otherwise false.</returns>
     public static bool IsErrorOrHigher(this ServiceLog log)
     {
@@ -26,6 +30,7 @@ public static class ServiceLogExtensions
     /// Determines whether this log entry represents a warning or higher severity level.
     /// </summary>
     /// <param name="log">The service log entry to check.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="log"/> is <see langword="null"/>.</exception>
     /// <returns>True if the log level is Warning, Error, Critical, Alert, or Emergency; otherwise false.</returns>
     public static bool IsWarningOrHigher(this ServiceLog log)
     {
@@ -37,6 +42,7 @@ public static class ServiceLogExtensions
     /// Gets a formatted string representation of the log entry including all key metadata.
     /// </summary>
     /// <param name="log">The service log entry to format.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="log"/> is <see langword="null"/>.</exception>
     /// <returns>A detailed formatted string with timestamp, unit name, level, and message.</returns>
     public static string ToDetailedString(this ServiceLog log)
     {
@@ -77,6 +83,7 @@ public static class ServiceLogExtensions
     /// </summary>
     /// <param name="log">The service log entry to check.</param>
     /// <param name="level">The severity level to match against.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="log"/> is <see langword="null"/>.</exception>
     /// <returns>True if the log level matches the specified level; otherwise false.</returns>
     public static bool HasLevel(this ServiceLog log, SyslogLevel level)
     {
@@ -88,6 +95,7 @@ public static class ServiceLogExtensions
     /// Gets a short summary string suitable for display in monitoring dashboards.
     /// </summary>
     /// <param name="log">The service log entry to summarize.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="log"/> is <see langword="null"/>.</exception>
     /// <returns>A concise summary string with the most important information.</returns>
     public static string ToSummaryString(this ServiceLog log)
     {
