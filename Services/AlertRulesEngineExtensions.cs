491458064bac3c6f1688864aa80bdc317bb5e12a
Vladyslav Zaiets <rutova2@gmail.com>
2022-12-17 18:14:16 +0200

    feat(services): add extension helpers for AlertRulesEngine

--- Services/AlertRulesEngineExtensions.cs
+++ Services/AlertRulesEngineExtensions.cs
@@ -18,11 +18,14 @@ public static class AlertRulesEngineExtensions
     /// <param name="severity">The severity level to filter by.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Filtered collection of alert rules.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
     public static async Task<IEnumerable<AlertRule>> GetRulesBySeverityAsync(
         this AlertRulesEngine engine,
         AlertSeverity severity,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+
         var rules = await engine.GetRulesAsync(cancellationToken);
         return rules.Where(r => r.Severity == severity).OrderBy(r => r.Name);
     }
@@ -34,11 +37,14 @@ public static class AlertRulesEngineExtensions
     /// <param name="severity">The severity level to filter by.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Filtered collection of active alert incidents.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
     public static async Task<IEnumerable<AlertIncident>> GetActiveIncidentsBySeverityAsync(
         this AlertRulesEngine engine,
         AlertSeverity severity,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+
         var incidents = await engine.GetActiveIncidentsAsync(cancellationToken);
         return incidents.Where(i => i.Severity == severity).OrderByDescending(i => i.CreatedAt);
     }
@@ -51,12 +57,17 @@ public static class AlertRulesEngineExtensions
     /// <param name="maxResults">Maximum number of results to return.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Collection of incidents for the specified service.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="serviceName"/> is null or empty.</exception>
     public static async Task<IEnumerable<AlertIncident>> GetIncidentsByServiceAsync(
         this AlertRulesEngine engine,
         string serviceName,
         int maxResults = 100,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+        ArgumentException.ThrowIfNullOrEmpty(serviceName);
+
         var incidents = await engine.GetIncidentHistoryAsync(serviceName, maxResults, cancellationToken);
         return incidents.OrderByDescending(i => i.CreatedAt);
     }
@@ -67,10 +78,13 @@ public static class AlertRulesEngineExtensions
     /// <param name="engine">The alert rules engine instance.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Dictionary mapping severity levels to active incident counts.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
     public static async Task<Dictionary<AlertSeverity, int>> GetActiveIncidentCountsBySeverityAsync(
         this AlertRulesEngine engine,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+
         var activeIncidents = await engine.GetActiveIncidentsAsync(cancellationToken);
         return activeIncidents
             .GroupBy(i => i.Severity)
@@ -84,11 +98,16 @@ public static class AlertRulesEngineExtensions
     /// <param name="servicePattern">The service pattern to match against.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Filtered collection of alert rules matching the pattern.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="servicePattern"/> is null or empty.</exception>
     public static async Task<IEnumerable<AlertRule>> GetRulesByServicePatternAsync(
         this AlertRulesEngine engine,
         string servicePattern,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+        ArgumentException.ThrowIfNullOrEmpty(servicePattern);
+
         var rules = await engine.GetRulesAsync(cancellationToken);
         return rules.Where(r => MatchesServicePattern(r.ServicePattern, servicePattern)).OrderBy(r => r.Name);
     }
@@ -100,11 +119,16 @@ public static class AlertRulesEngineExtensions
     /// <param name="serviceName">Name of the service to check.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>The most recent active incident, or null if none exists.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="serviceName"/> is null or empty.</exception>
     public static async Task<AlertIncident?> GetLatestIncidentForServiceAsync(
         this AlertRulesEngine engine,
         string serviceName,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+        ArgumentException.ThrowIfNullOrEmpty(serviceName);
+
         var incidents = await engine.GetIncidentsByServiceAsync(serviceName, 1, cancellationToken);
         return incidents.FirstOrDefault();
     }
@@ -115,10 +139,13 @@ public static class AlertRulesEngineExtensions
     /// <param name="engine">The alert rules engine instance.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Collection of unacknowledged active incidents.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
     public static async Task<IEnumerable<AlertIncident>> GetUnacknowledgedActiveIncidentsAsync(
         this AlertRulesEngine engine,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+
         var activeIncidents = await engine.GetActiveIncidentsAsync(cancellationToken);
         return activeIncidents.Where(i => i.State != AlertIncidentState.Acknowledged).OrderByDescending(i => i.CreatedAt);
     }
@@ -129,10 +156,13 @@ public static class AlertRulesEngineExtensions
     /// <param name="engine">The alert rules engine instance.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Collection of escalated incidents.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
     public static async Task<IEnumerable<AlertIncident>> GetEscalatedIncidentsAsync(
         this AlertRulesEngine engine,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+
         var activeIncidents = await engine.GetActiveIncidentsAsync(cancellationToken);
         return activeIncidents.Where(i => i.State == AlertIncidentState.Escalated).OrderByDescending(i => i.CreatedAt);
     }
@@ -143,18 +173,31 @@ public static class AlertRulesEngineExtensions
     /// <param name="engine">The alert rules engine instance.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
     /// <returns>Dictionary mapping incident states to counts.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="engine"/> is null.</exception>
     public static async Task<Dictionary<AlertIncidentState, int>> GetIncidentCountsByStateAsync(
         this AlertRulesEngine engine,
         CancellationToken cancellationToken = default)
     {
+        ArgumentNullException.ThrowIfNull(engine);
+
         var activeIncidents = await engine.GetActiveIncidentsAsync(cancellationToken);
         return activeIncidents
             .GroupBy(i => i.State)
             .ToDictionary(g => g.Key, g => g.Count());
     }
 
+    /// <summary>
+    /// Determines whether a service name matches a given service pattern.
+    /// </summary>
+    /// <param name="pattern">The pattern to match against (supports wildcards).</param>
+    /// <param name="serviceName">The service name to check.</param>
+    /// <returns>True if the service name matches the pattern; otherwise, false.</returns>
+    /// <exception cref="ArgumentException">Thrown if <paramref name="pattern"/> or <paramref name="serviceName"/> is null or empty.</exception>
     private static bool MatchesServicePattern(string pattern, string serviceName)
     {
+        ArgumentException.ThrowIfNullOrEmpty(pattern);
+        ArgumentException.ThrowIfNullOrEmpty(serviceName);
+
         if (pattern == "*")
             return true;
 
