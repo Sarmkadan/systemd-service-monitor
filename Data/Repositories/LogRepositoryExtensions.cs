5d96f400b2ef510a33c20a2656bd08064ba0d96b
Vladyslav Zaiets <rutova2@gmail.com>
2022-12-21 07:53:16 +0200

    feat: nits

--- Data/Repositories/LogRepositoryExtensions.cs
+++ Data/Repositories/LogRepositoryExtensions.cs
@@ -1,11 +1,12 @@
 #nullable enable
 
+using System;
 using SystemdServiceMonitor.Models;
 
 namespace SystemdServiceMonitor.Data.Repositories;
 
 /// <summary>
-/// Extension methods for LogRepository providing additional utility operations.
+/// Extension methods for <see cref="LogRepository"/> providing additional utility operations.
 /// </summary>
 public static class LogRepositoryExtensions
 {
@@ -13,6 +14,9 @@ public static class LogRepositoryExtensions
     /// Gets logs filtered by service name with optional time range and level filtering.
     /// </summary>
     /// <param name="repository">The log repository instance</param>
+    /// <exception cref="ArgumentNullException"><paramref name="repository"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="serviceName"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentException"><paramref name="serviceName"/> is empty or whitespace</exception>
     /// <param name="serviceName">The service name to filter by</param>
     /// <param name="timeRange">Optional time range to filter logs within</param>
     /// <param name="level">Optional log level to filter by</param>
@@ -27,6 +31,13 @@ public static class LogRepositoryExtensions
         int limit = 100,
         CancellationToken ct = default)
     {
+        ArgumentNullException.ThrowIfNull(repository);
+        ArgumentNullException.ThrowIfNull(serviceName);
+        if (string.IsNullOrWhiteSpace(serviceName))
+        {
+            throw new ArgumentException("Service name cannot be empty or whitespace.", nameof(serviceName));
+        }
+
         var logs = await repository.GetByUnitNameAsync(serviceName, limit, ct);
 
         if (timeRange.HasValue)
@@ -47,6 +58,7 @@ public static class LogRepositoryExtensions
     /// Gets the most recent log entry for a specific service.
     /// </summary>
     /// <param name="repository">The log repository instance</param>
+    /// <exception cref="ArgumentNullException"><paramref name="repository"/> is <see langword="null"/></exception>
     /// <param name="serviceId">The service ID to get the latest log for</param>
     /// <param name="ct">Cancellation token</param>
     /// <returns>The most recent log entry, or null if none exists</returns>
@@ -55,6 +67,8 @@ public static class LogRepositoryExtensions
         Guid serviceId,
         CancellationToken ct = default)
     {
+        ArgumentNullException.ThrowIfNull(repository);
+
         var logs = await repository.GetByServiceIdAsync(serviceId, limit: 1, ct);
         return logs.FirstOrDefault();
     }
@@ -63,31 +77,32 @@ public static class LogRepositoryExtensions
     /// Gets logs grouped by their level with counts for each level.
     /// </summary>
     /// <param name="repository">The log repository instance</param>
+    /// <exception cref="ArgumentNullException"><paramref name="repository"/> is <see langword="null"/></exception>
     /// <param name="ct">Cancellation token</param>
     /// <returns>Dictionary mapping log levels to their respective log counts</returns>
     public static async Task<Dictionary<SyslogLevel, int>> GetLogLevelCountsAsync(
         this LogRepository repository,
         CancellationToken ct = default)
     {
-        var allLogs = await repository.GetCountAsync(ct);
-        var levelLogs = await repository.GetByLevelAsync(SyslogLevel.Debug, ct);
-        var errorLogs = await repository.GetByLevelAsync(SyslogLevel.Error, ct);
-        var infoLogs = await repository.GetByLevelAsync(SyslogLevel.Info, ct);
-        var warningLogs = await repository.GetByLevelAsync(SyslogLevel.Warning, ct);
+        ArgumentNullException.ThrowIfNull(repository);
 
-        return new Dictionary<SyslogLevel, int>
+        var result = new Dictionary<SyslogLevel, int>();
+
+        foreach (SyslogLevel level in Enum.GetValues(typeof(SyslogLevel)))
         {
-            [SyslogLevel.Debug] = await repository.GetByLevelAsync(SyslogLevel.Debug, ct).ContinueWith(t => t.Result.Count()),
-            [SyslogLevel.Error] = await repository.GetByLevelAsync(SyslogLevel.Error, ct).ContinueWith(t => t.Result.Count()),
-            [SyslogLevel.Info] = await repository.GetByLevelAsync(SyslogLevel.Info, ct).ContinueWith(t => t.Result.Count()),
-            [SyslogLevel.Warning] = await repository.GetByLevelAsync(SyslogLevel.Warning, ct).ContinueWith(t => t.Result.Count())
-        };
+            var logs = await repository.GetByLevelAsync(level, ct);
+            result[level] = logs.Count();
+        }
+
+        return result;
     }
 
     /// <summary>
     /// Gets logs that match any of the specified log levels.
     /// </summary>
     /// <param name="repository">The log repository instance</param>
+    /// <exception cref="ArgumentNullException"><paramref name="repository"/> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="levels"/> is <see langword="null"/></exception>
     /// <param name="levels">Collection of log levels to filter by</param>
     /// <param name="ct">Cancellation token</param>
     /// <returns>Logs matching any of the specified levels</returns>
@@ -96,6 +111,9 @@ public static class LogRepositoryExtensions
         IEnumerable<SyslogLevel> levels,
         CancellationToken ct = default)
     {
+        ArgumentNullException.ThrowIfNull(repository);
+        ArgumentNullException.ThrowIfNull(levels);
+
         var result = new List<ServiceLog>();
 
         foreach (var level in levels.Distinct())
