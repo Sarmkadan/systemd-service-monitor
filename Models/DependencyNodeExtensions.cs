aafccd39c5a651c1f81f826d04eabec46668aeff
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 18:21:13 +0300

    refactor: small fix

--- Models/DependencyNodeExtensions.cs
+++ Models/DependencyNodeExtensions.cs
@@ -14,8 +14,8 @@ public static class DependencyNodeExtensions
     /// Checks if the node has any dependencies.
     /// </summary>
     /// <param name="node">The <see cref="DependencyNode"/>.</param>
-    /// <returns>True if the node has dependencies, false otherwise.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="node"/> is null.</exception>
+    /// <returns><see langword="true"/> if the node has dependencies; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="node"/> is <see langword="null"/>.</exception>
     public static bool HasDependencies(this DependencyNode node)
     {
         ArgumentNullException.ThrowIfNull(node);
@@ -26,8 +26,8 @@ public static class DependencyNodeExtensions
     /// Checks if the node has any dependents.
     /// </summary>
     /// <param name="node">The <see cref="DependencyNode"/>.</param>
-    /// <returns>True if the node has dependents, false otherwise.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="node"/> is null.</exception>
+    /// <returns><see langword="true"/> if the node has dependents; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="node"/> is <see langword="null"/>.</exception>
     public static bool HasDependents(this DependencyNode node)
     {
         ArgumentNullException.ThrowIfNull(node);
@@ -38,8 +38,8 @@ public static class DependencyNodeExtensions
     /// Determines if the node is isolated (no dependencies and no dependents).
     /// </summary>
     /// <param name="node">The <see cref="DependencyNode"/>.</param>
-    /// <returns>True if the node is isolated, false otherwise.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="node"/> is null.</exception>
+    /// <returns><see langword="true"/> if the node is isolated; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="node"/> is <see langword="null"/>.</exception>
     public static bool IsIsolated(this DependencyNode node)
     {
         ArgumentNullException.ThrowIfNull(node);
@@ -50,12 +50,17 @@ public static class DependencyNodeExtensions
     /// Gets a descriptive string summary of the node.
     /// </summary>
     /// <param name="node">The <see cref="DependencyNode"/>.</param>
-    /// <returns>A string summary.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="node"/> is null.</exception>
+    /// <returns>A string summary containing the service name, state, and dependency counts.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="node"/> is <see langword="null"/>.</exception>
     public static string GetSummary(this DependencyNode node)
     {
         ArgumentNullException.ThrowIfNull(node);
-        return string.Format(CultureInfo.InvariantCulture, "{0} [{1}] (Deps: {2}, Dependents: {3})", 
-            node.ServiceName, node.State, node.Dependencies.Count, node.Dependents.Count);
+        return string.Format(
+            CultureInfo.InvariantCulture,
+            "{0} [{1}] (Deps: {2}, Dependents: {3})",
+            node.ServiceName,
+            node.State,
+            node.Dependencies.Count,
+            node.Dependents.Count);
     }
 }
