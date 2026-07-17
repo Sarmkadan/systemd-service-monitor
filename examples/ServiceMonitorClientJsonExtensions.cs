0e527f4fca416f0dfa19a1bf74393048bf951563
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 19:09:23 +0300

    readability pass over ServiceMonitorClientJsonExtensions

--- examples/ServiceMonitorClientJsonExtensions.cs
+++ examples/ServiceMonitorClientJsonExtensions.cs
@@ -6,7 +6,7 @@ using System.Text.Json;
 namespace SystemdMonitorExamples
 {
     /// <summary>
-    /// Provides System.Text.Json serialization extensions for <see cref="ServiceMonitorClient"/>
+    /// Provides System.Text.Json serialization extensions for <see cref="ServiceMonitorClient"/>.
     /// </summary>
     public static class ServiceMonitorClientJsonExtensions
     {
@@ -20,10 +20,10 @@ namespace SystemdMonitorExamples
         /// <summary>
         /// Serializes the <see cref="ServiceMonitorClient"/> instance to a JSON string.
         /// </summary>
-        /// <param name="value">The client instance to serialize</param>
-        /// <param name="indented">Whether to format the JSON with indentation</param>
-        /// <returns>A JSON string representation of the client</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null</exception>
+        /// <param name="value">The client instance to serialize.</param>
+        /// <param name="indented">Whether to format the JSON with indentation.</param>
+        /// <returns>A JSON string representation of the client.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
         public static string ToJson(this ServiceMonitorClient value, bool indented = false)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -34,30 +34,24 @@ namespace SystemdMonitorExamples
         /// <summary>
         /// Deserializes a JSON string to a <see cref="ServiceMonitorClient"/> instance.
         /// </summary>
-        /// <param name="json">The JSON string to deserialize</param>
-        /// <returns>A deserialized client instance, or null if the JSON is invalid</returns>
-        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty</exception>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <returns>A deserialized client instance, or null if deserialization fails.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+        /// <exception cref="JsonException">Thrown when the JSON is invalid and cannot be deserialized.</exception>
         public static ServiceMonitorClient? FromJson(string json)
         {
             ArgumentException.ThrowIfNullOrEmpty(json);
 
-            try
-            {
-                return JsonSerializer.Deserialize<ServiceMonitorClient>(json, _jsonOptions);
-            }
-            catch (JsonException)
-            {
-                return null;
-            }
+            return JsonSerializer.Deserialize<ServiceMonitorClient>(json, _jsonOptions);
         }
 
         /// <summary>
         /// Attempts to deserialize a JSON string to a <see cref="ServiceMonitorClient"/> instance.
         /// </summary>
-        /// <param name="json">The JSON string to deserialize</param>
-        /// <param name="value">Receives the deserialized client instance if successful</param>
-        /// <returns>True if deserialization succeeded; otherwise, false</returns>
-        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty</exception>
+        /// <param name="json">The JSON string to deserialize.</param>
+        /// <param name="value">Receives the deserialized client instance if successful.</param>
+        /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
         public static bool TryFromJson(string json, out ServiceMonitorClient? value)
         {
             ArgumentException.ThrowIfNullOrEmpty(json);
